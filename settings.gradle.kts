6af664f9b2b33c50bbcc052403a9936b09b14d80
aking <akingyin@163.com>
2024-07-24 10:29:43 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        maven {setUrl("https://mirrors.huaweicloud.com/repository/maven")}
+
+        maven {setUrl("https://maven.scijava.org/content/repositories/public")}
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven {setUrl("https://mirrors.huaweicloud.com/repository/maven")}
+        maven {setUrl("https://maven.scijava.org/content/repositories/public")}
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TestK2"
+include(":app")
+ 
